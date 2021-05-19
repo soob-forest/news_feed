53e4c387fc5ac7a134edb8bd5885283cacf86308
soob-forest <dltntnqo@naver.com>
2021-05-20 00:18:13 +0900

    FEAT] user school manage 엔티티는 graphql 스키마에서 제외

--- src/user-school-manage/entites/user-school-manage.entity.ts
+++ src/user-school-manage/entites/user-school-manage.entity.ts
@@ -2,15 +2,15 @@ import { Field } from '@nestjs/graphql';
 import { CoreEntity } from 'src/common/entities/core.entity';
 import { School } from 'src/schools/entities/schools.entity';
 import { User } from 'src/users/entities/users.entity';
-import { Entity, ManyToOne } from 'typeorm';
+import { Entity, ManyToOne, PrimaryGeneratedColumn } from 'typeorm';
 
 @Entity()
-export class UserSchoolManage extends CoreEntity {
+export class UserSchoolManage {
+  @PrimaryGeneratedColumn()
+  id: number;
   @ManyToOne((type) => User, (user) => user.userSchoolFollow)
-  @Field((type) => User, { nullable: true })
   user?: User;
 
   @ManyToOne((type) => School, (school) => school.userSchoolFollow)
-  @Field((type) => School, { nullable: true })
   school?: School;
 }
