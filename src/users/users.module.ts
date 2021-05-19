7eb061aed0b978f077a5bc6e93a90b933eea853a
soob-forest <dltntnqo@naver.com>
2021-05-19 22:50:27 +0900

    FEAT] usermodule 필요한 provider import, export

--- src/users/users.module.ts
+++ src/users/users.module.ts
@@ -1,4 +1,27 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { JwtModule } from '@nestjs/jwt';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { User } from './entities/users.entity';
+import { UserResolver } from './users.resolver';
+import { UserService } from './users.service';
 
-@Module({})
+@Module({
+  imports: [
+    TypeOrmModule.forFeature([User]),
+    JwtModule.registerAsync({
+      imports: [ConfigModule],
+      inject: [ConfigService],
+      useFactory: async (configService: ConfigService) => ({
+        secret: configService.get('JWT_SECRET'),
+        signOptions: {
+          expiresIn: `${configService.get('JWT_EXPIRATION_TIME')}d`,
+        },
+      }),
+    }),
+    ConfigService,
+  ],
+  providers: [UserResolver, UserService],
+  exports: [UserService],
+})
 export class UsersModule {}
