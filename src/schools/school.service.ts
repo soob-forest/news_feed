db50983a0b25acfc83e6f24eb082aec4dd66aa09
soob-forest <dltntnqo@naver.com>
2021-05-20 00:15:11 +0900

    FEAT] 학교 생성 서비스

---
+++ src/schools/school.service.ts
@@ -0,0 +1,60 @@
+import { Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { UserSchoolManage } from 'src/user-school-manage/entites/user-school-manage.entity';
+import { User } from 'src/users/entities/users.entity';
+import { Connection, Repository } from 'typeorm';
+import {
+  CreateSchoolInput,
+  CreateSchoolOutput,
+} from './dtos/create-school.dto';
+import { School } from './entities/schools.entity';
+
+@Injectable()
+export class SchoolService {
+  constructor(
+    @InjectRepository(School) private readonly school: Repository<School>,
+    @InjectRepository(UserSchoolManage)
+    private readonly userSchoolManage: Repository<UserSchoolManage>,
+    private readonly connection: Connection,
+  ) {}
+
+  async createSchool(
+    user: User,
+    { name, address }: CreateSchoolInput,
+  ): Promise<CreateSchoolOutput> {
+    const queryRunner = this.connection.createQueryRunner();
+
+    await queryRunner.connect();
+    await queryRunner.startTransaction();
+
+    try {
+      const exists = await this.school.findOne({ name, address });
+
+      if (exists) {
+        return { ok: false, error: 'There is school already' };
+      }
+
+      const school = await queryRunner.manager.save(
+        this.school.create({ name, address }),
+      );
+      await queryRunner.manager.save(
+        this.userSchoolManage.create({ user, school }),
+      );
+
+      await queryRunner.commitTransaction();
+
+      return {
+        ok: true,
+        schoolId: school.id,
+      };
+    } catch (error) {
+      await queryRunner.rollbackTransaction();
+      return {
+        ok: false,
+        error: "Can't create school",
+      };
+    } finally {
+      await queryRunner.release();
+    }
+  }
+}
