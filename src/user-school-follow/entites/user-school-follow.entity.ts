548d770e88c5797d776e729270311e890f1f21ba
soob-forest <dltntnqo@naver.com>
2021-05-20 00:17:28 +0900

    FEAT] user school follow 엔티티는 graphql 스키마에서 제외

--- src/user-school-follow/entites/user-school-follow.entity.ts
+++ src/user-school-follow/entites/user-school-follow.entity.ts
@@ -3,30 +3,28 @@ import { IsBoolean, IsDate } from 'class-validator';
 import { CoreEntity } from 'src/common/entities/core.entity';
 import { School } from 'src/schools/entities/schools.entity';
 import { User } from 'src/users/entities/users.entity';
-import { Column, Entity, ManyToOne } from 'typeorm';
+import { Column, Entity, ManyToOne, PrimaryGeneratedColumn } from 'typeorm';
 
 @Entity()
-export class UserSchoolFollow extends CoreEntity {
+export class UserSchoolFollow {
+  @PrimaryGeneratedColumn()
+  id: number;
+
   @Column()
-  @Field((type) => String, { nullable: true })
   @IsDate()
   startDatetime?: Date;
 
   @Column()
-  @Field((type) => String, { nullable: true })
   @IsDate()
   cancelDatetime?: Date;
 
   @Column()
-  @Field((type) => Boolean, { nullable: true })
   @IsBoolean()
   isCancel?: boolean;
 
   @ManyToOne((type) => User, (user) => user.userSchoolFollow)
-  @Field((type) => User, { nullable: true })
   user?: User;
 
   @ManyToOne((type) => School, (school) => school.userSchoolFollow)
-  @Field((type) => School, { nullable: true })
   school?: School;
 }
