3a0036ba13e465b7e0c9dc31374740ada8761f95
soob-forest <dltntnqo@naver.com>
2021-05-20 00:18:37 +0900

    FEAT] school 모듈 import, provider 정리

--- src/schools/schools.module.ts
+++ src/schools/schools.module.ts
@@ -1,4 +1,12 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { UserSchoolManage } from 'src/user-school-manage/entites/user-school-manage.entity';
+import { School } from './entities/schools.entity';
+import { SchoolResolver } from './school.resolver';
+import { SchoolService } from './school.service';
 
-@Module({})
+@Module({
+  imports: [TypeOrmModule.forFeature([School, UserSchoolManage])],
+  providers: [SchoolResolver, SchoolService],
+})
 export class SchoolsModule {}
