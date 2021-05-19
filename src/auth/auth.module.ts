f1f0f56b32870bf0bc868281911a9a827dc0b5be
soob-forest <dltntnqo@naver.com>
2021-05-19 22:54:01 +0900

    FEAT] 인증, 권한을 위해 usermodule import

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -1,9 +1,9 @@
 import { Module } from '@nestjs/common';
-import { AuthService } from './auth.service';
 import { JwtModule } from '@nestjs/jwt';
 import { AuthGuard } from './auth.guard';
 import { APP_GUARD } from '@nestjs/core';
 import { ConfigModule, ConfigService } from '@nestjs/config';
+import { UsersModule } from 'src/users/users.module';
 
 @Module({
   imports: [
@@ -18,14 +18,13 @@ import { ConfigModule, ConfigService } from '@nestjs/config';
       }),
     }),
     ConfigService,
+    UsersModule,
   ],
   providers: [
-    AuthService,
     {
       provide: APP_GUARD,
       useClass: AuthGuard,
     },
   ],
-  exports: [AuthService],
 })
 export class AuthModule {}
