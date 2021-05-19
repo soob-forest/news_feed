ed3335fe7cb2426ac190fba2c4436102c60b62f6
soob-forest <dltntnqo@naver.com>
2021-05-19 21:01:46 +0900

    FEAT] login resolver

--- src/users/users.resolver.ts
+++ src/users/users.resolver.ts
@@ -10,6 +10,7 @@ import {
   CreateAccountInput,
   CreateAccountOutput,
 } from './dtos/create-account.dto';
+import { LoginInput, LoginOutput } from './dtos/login.dto';
 import { User } from './entities/users.entity';
 import { UserService } from './users.service';
 
@@ -22,5 +23,10 @@ export class UserResolver {
     @Args('input') createAccountInput: CreateAccountInput,
   ): Promise<CreateAccountOutput> {
     return this.userService.createAccount(createAccountInput);
+  }
+
+  @Mutation((returns) => LoginOutput)
+  async login(@Args('input') loginInput: LoginInput): Promise<LoginOutput> {
+    return this.userService.login(loginInput);
   }
 }
