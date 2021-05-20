5c3b7c5ae973af1f430c70410ade50ba765cbdd1
soob-forest <dltntnqo@naver.com>
2021-05-21 01:49:08 +0900

    TEST] 계정 생성 unit test

---
+++ src/users/users.service.spec.ts
@@ -0,0 +1,112 @@
+import { JwtService } from '@nestjs/jwt';
+import { Test } from '@nestjs/testing';
+import { getRepositoryToken } from '@nestjs/typeorm';
+import { News } from 'src/news/entites/news.entity';
+import { School } from 'src/schools/entities/schools.entity';
+import { UserSchoolFollow } from 'src/user-school-follow/entites/user-school-follow.entity';
+import { UserSchoolManage } from 'src/user-school-manage/entites/user-school-manage.entity';
+import { Repository } from 'typeorm';
+import { Role, User } from './entities/users.entity';
+import { UserService } from './users.service';
+
+const mockRepository = () => ({
+  findOne: jest.fn(),
+  save: jest.fn(),
+  create: jest.fn(),
+  findOneOrFail: jest.fn(),
+  delete: jest.fn(),
+});
+
+const mockJwtService = () => ({
+  sign: jest.fn(() => 'token'),
+  verify: jest.fn(),
+});
+
+type MockRepository<T = any> = Partial<Record<keyof Repository<T>, jest.Mock>>;
+
+describe('UserService', () => {
+  let service: UserService;
+  let usersRepository: MockRepository<User>;
+  let jwtService: JwtService;
+
+  beforeEach(async () => {
+    const module = await Test.createTestingModule({
+      providers: [
+        UserService,
+        {
+          provide: getRepositoryToken(User),
+          useValue: mockRepository(),
+        },
+        {
+          provide: JwtService,
+          useValue: mockJwtService(),
+        },
+        {
+          provide: getRepositoryToken(UserSchoolManage),
+          useValue: mockRepository(),
+        },
+        {
+          provide: getRepositoryToken(UserSchoolFollow),
+          useValue: mockRepository(),
+        },
+        {
+          provide: getRepositoryToken(School),
+          useValue: mockRepository(),
+        },
+        {
+          provide: getRepositoryToken(News),
+          useValue: mockRepository(),
+        },
+      ],
+    }).compile();
+    service = module.get<UserService>(UserService);
+    jwtService = module.get<JwtService>(JwtService);
+    usersRepository = module.get(getRepositoryToken(User));
+  });
+
+  it('should be defined', () => {
+    expect(service).toBeDefined();
+  });
+
+  describe('createAccount', () => {
+    const createAccountArgs = {
+      email: 'test@email.com',
+      password: '1234',
+      role: Role.STUDENT,
+    };
+
+    it('should fail if user exists', async () => {
+      usersRepository.findOne.mockResolvedValue({
+        id: 1,
+        email: '',
+      });
+      const result = await service.createAccount(createAccountArgs);
+      expect(result).toMatchObject({
+        ok: false,
+        error: 'There is email already',
+      });
+    });
+
+    it('should create a new user', async () => {
+      usersRepository.findOne.mockResolvedValue(undefined);
+      usersRepository.create.mockReturnValue(createAccountArgs);
+      usersRepository.save.mockResolvedValue(createAccountArgs);
+
+      const result = await service.createAccount(createAccountArgs);
+
+      expect(usersRepository.create).toHaveBeenCalledTimes(1);
+      expect(usersRepository.create).toHaveBeenCalledWith(createAccountArgs);
+
+      expect(usersRepository.save).toHaveBeenCalledTimes(1);
+      expect(usersRepository.save).toHaveBeenCalledWith(createAccountArgs);
+
+      expect(result).toEqual({ ok: true });
+    });
+
+    it('should fail on exception', async () => {
+      usersRepository.findOne.mockRejectedValue(new Error());
+      const result = await service.createAccount(createAccountArgs);
+      expect(result).toEqual({ ok: false, error: "Couldn't create account" });
+    });
+  });
+});
