359b38e4109fa0ce957a7571e983089b2bc49aa0
soob-forest <dltntnqo@naver.com>
2021-05-23 23:25:49 +0900

    FEAT] 앱 시작 설정

--- src/app.module.ts
+++ src/app.module.ts
@@ -19,7 +19,7 @@ import { AuthModule } from './auth/auth.module';
   imports: [
     ConfigModule.forRoot({
       isGlobal: true,
-      envFilePath: process.env.NODE_ENV === 'dev' ? '.env' : '.env.test',
+      envFilePath: process.env.NODE_ENV !== 'test' ? '.env' : '.env.test',
       validationSchema: Joi.object({
         NODE_ENV: Joi.string().valid('dev', 'prod').required(),
         DB_HOST: Joi.string().required(),
